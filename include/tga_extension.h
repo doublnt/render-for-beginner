abbbaf29c3b5a46bf5a7cff68d5782fccb538b29
doublnt <yinximail@gmail.com>
2022-08-13 15:41:37 +0800

    triangle generate add.

--- include/tga_extension.h
+++ include/tga_extension.h
@@ -4,8 +4,13 @@
 #include "model.h"
 #include "tgaimage.h"
 
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor custom_pink = TGAColor(238, 100, 40, 255);
+const TGAColor custom_green = TGAColor(58, 255, 78, 255);
+
 // 布雷森汉姆直线算法
-void plot_line(int x0, int y0, int x1, int y1, TGAImage& tga_image,
+void line(int x0, int y0, int x1, int y1, TGAImage& tga_image,
                const TGAColor& color) {
   bool steep = std::abs(y1 - y0) > std::abs(x1 - x0);
 
@@ -47,7 +52,12 @@ void plot_line(int x0, int y0, int x1, int y1, TGAImage& tga_image,
   }
 }
 
-void plot_face(Model* model, TGAImage& tga_image, int width, int height,
+void line(const Vec2i& t0, const Vec2i& t1, TGAImage& tga_image,
+               const TGAColor& color) {
+  line(t0.x, t0.y, t1.x, t1.y, tga_image, color);
+}
+
+void face(Model* model, TGAImage& tga_image, int width, int height,
                const TGAColor& color) {
   int nfaces_count = model->nfaces();
   for (int i = 0; i < nfaces_count; ++i) {
@@ -63,11 +73,49 @@ void plot_face(Model* model, TGAImage& tga_image, int width, int height,
       int x1 = (v1.x + 1.0) * width / 2.0;
       int y1 = (v1.y + 1.0) * height / 2.0;
 
-      plot_line(x0, y0, x1, y1, tga_image, color);
+      line(x0, y0, x1, y1, tga_image, color);
     }
   }
-
-  tga_image.flip_vertically();
 }
 
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& tga_image,
+                   const TGAColor& color) {
+  if (t0.y == t1.y && t0.y == t2.y) {
+    return;
+  }
+
+  if (t0.y > t1.y) {
+    std::swap(t0, t1);
+  }
+
+  if (t0.y > t2.y) {
+    std::swap(t0, t2);
+  }
+
+  if (t1.y > t2.y) {
+    std::swap(t1, t2);
+  }
+
+  int total_height = t2.y - t0.y;
+
+  for (int i = 0; i < total_height; ++i) {
+    bool second_half = i > (t1.y - t0.y) || t1.y == t0.y;
+    int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+
+    float alpha = static_cast<float>(i / total_height);
+    float beta = static_cast<float>(i - (second_half ? t1.y - t0.y : 0)) /
+                 segment_height;
+
+    Vec2i a = t0 + (t2 - t0) * alpha;
+    Vec2i b = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+
+    if (a.x > b.x) {
+      std::swap(a, b);
+    }
+
+    for (int j = a.x; j <= b.x; ++j) {
+      tga_image.set(j, t0.y + i, color);
+    }
+  }
+}
 #endif  // __TGA_EXTENSION_H__
--- render_run_main.cc
+++ render_run_main.cc
@@ -1,13 +1,8 @@
 #include <../include/tga_extension.h>
 #include <../include/tgaimage.h>
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
-const TGAColor custom_pink = TGAColor(238, 100, 40, 255);
-const TGAColor custom_green = TGAColor(58, 255, 78, 255);
-
-const int width = 800;
-const int height = 800;
+const int width = 400;
+const int height = 400;
 
 Model* model = nullptr;
 
@@ -15,8 +10,16 @@ int main(int argc, char** argv) {
   TGAImage image(width, height, TGAImage::RGB);
 
   model = new Model("d://work//render-for-beginner//obj//human_head.obj");
-  plot_face(model, image, width, height, white);
-  image.flip_vertically();
+  // face(model, image, width, height, white);
+
+  Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
+  Vec2i t1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
+  Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+
+  triangle(t0[0], t0[1], t0[2], image, red);
+  triangle(t1[0], t1[1], t1[2], image, white);
+  triangle(t2[0], t2[1], t2[2], image, custom_green);
+
   image.write_tga_file("d://work//render-for-beginner//output//output.tga");
 
   delete model;
